6ff2f4f98a6fe1b3085330887741ab7fe113a4c8
akdeepak <akdeepak85@gmail.com>
2025-02-21 06:06:21 +0000

    Update test.py

--- test.py
+++ test.py
@@ -11,9 +11,17 @@ def test_predict():
     """
     Test the predict route with test data
     """
-    test_description = {"description": "this is a test description about Dementia"}
+    long_text = "This is a long document that exceeds the token limit of 512. " * 50
+
+    # test_description = {"description": "this is a test description about Dementia"}
+
+    # test_description = {"description": "This is a long document that exceeds the token limit of 512. " * 50}
     print("Calling API with test description:")
+    test_description = {"description": "With the development of supportive measures, the natural history of ALS has changed. Researchers compared the natural history of ALS patients from 1999-2004 and 1984-1998 and found that the median survival time was significantly longer in the former than in the latter (4.32 years vs. 3.22 years) and that the disease progression was slower in the former, even after adjusting for other confounding factors. Although previous studies have provided reference for the diagnosis and treatment of ALS, the etiology of ALS is still unknown, and the relevant clinical features and natural history of ALS still lack the verification of large samples. Therefore, the research on the natural history of ALS is of great significance to further increase the understanding of ALS and provide new evidence for the diagnosis and treatment of ALS."}
+    print("Calling API with test description:")
+    # response = requests.post(f"{BASE_URL}/predict", data=json.dumps(test_description))
     response = requests.post(f"{BASE_URL}/predict", data=json.dumps(test_description))
+
     print("Response: ")
     print(response.status_code)
     print(response.json())
